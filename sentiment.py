f480603df7c2e3e6ae43c5df1110b2fc750d3efb
Friedemann Ang <friedemann.ang@datavlt.com>
2018-08-23 18:45:42 +0800

    Notebook showing how to load and use a serialized trained model

---
+++ sentiment.py
@@ -0,0 +1,121 @@
+from sklearn import metrics
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+import gensim
+import spacy
+nlp = spacy.load('en')
+
+""" 
+1. Load the model
+2. Load the word indices for that model
+3. Evaluate
+4. Predict
+"""
+
+def load_fasttext(path, weights):
+	INPUT_DIM = weights.shape[0]
+	EMBEDDING_DIM = weights.shape[1]
+	OUTPUT_DIM = 1
+
+	model = FastText(INPUT_DIM, EMBEDDING_DIM, OUTPUT_DIM)
+	model.load_state_dict(torch.load(path)['state_dict'])
+
+	device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+	model = model.to(device)
+
+	print(model)
+	model.eval()
+	
+	return model
+
+""" Helper functions and classes below this point """
+
+class FastText(nn.Module):
+    def __init__(self, vocab_size, embedding_dim, output_dim):
+        super().__init__()
+        
+        self.embedding = nn.Embedding(vocab_size, embedding_dim)
+        self.fc = nn.Linear(embedding_dim, output_dim)
+        
+    def forward(self, x):
+        
+        #x = [sent len, batch size]
+        
+        embedded = self.embedding(x)
+                
+        #embedded = [sent len, batch size, emb dim]
+        
+        embedded = embedded.permute(1, 0, 2)
+        
+        #embedded = [batch size, sent len, emb dim]
+        
+        pooled = F.avg_pool2d(embedded, (embedded.shape[1], 1)).squeeze(1) 
+        
+        #pooled = [batch size, embedding_dim]
+                
+        return self.fc(pooled)
+
+def binary_accuracy(preds, y):
+    """
+    Returns accuracy per batch, i.e. if you get 8/10 right, this returns 0.8, NOT 8
+    """
+
+    #round predictions to the closest integer
+    rounded_preds = torch.round(F.sigmoid(preds))
+    correct = (rounded_preds == y).float() #convert into float for division 
+    acc = correct.sum()/len(correct)
+    return acc
+
+
+def train(model, iterator, optimizer, criterion):
+    
+    epoch_loss = 0
+    epoch_acc = 0
+    
+    model.train()
+    
+    for batch in iterator:
+
+        optimizer.zero_grad()
+        
+        predictions = model(batch.text).squeeze(1)
+        
+        loss = criterion(predictions, batch.label)
+        
+        acc = binary_accuracy(predictions, batch.label)
+        
+        loss.backward()
+        
+        optimizer.step()
+        
+        epoch_loss += loss.item()
+        epoch_acc += acc.item()
+        
+    return epoch_loss / len(iterator), epoch_acc / len(iterator)
+
+def evaluate(model, iterator, criterion):
+    
+    epoch_loss = 0
+    epoch_acc = 0
+    
+    model.eval()
+    
+    with torch.no_grad():
+    
+        for batch in iterator:
+
+            predictions = model(batch.text).squeeze(1)
+            
+            loss = criterion(predictions, batch.label)
+            
+            acc = binary_accuracy(predictions, batch.label)
+
+            epoch_loss += loss.item()
+            epoch_acc += acc.item()
+        
+    return epoch_loss / len(iterator), epoch_acc / len(iterator)
